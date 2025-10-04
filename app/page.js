c03171bdff5e409e0753ab51edc53f108890a4e1
fischerhewitt <fihewitt@calpoly.edu>
2025-10-03 17:08:57 -0700

    Test auto-deploy

--- app/page.js
+++ app/page.js
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main className="min-h-screen grid place-items-center bg-gray-50">
-      <h1 className="text-4xl font-bold text-blue-600">Hello Tailwind + Next.js!</h1>
+      <h1 className="text-4xl font-bold text-blue-600">Hello Tailwind + Next.js (v2)!</h1>
     </main>
   )
 }
